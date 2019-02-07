f57daf0c2de9259445527ff4d7bdf062e65a34fb
Chetan Kumar <ckumar@tataunistore.com>
2019-02-07 23:33:35 +0530

    inital commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const fs = require("fs");
+const hbs = require("hbs");
+
+var app = express();
+
+app.set("view engine", "hbs");
+
+
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamIt", text => {
+  return text.toUpperCase();
+});
+app.get("/", (req, res) => {
+  res.render("home.hbs", {
+    pageTitle: "Home Page",
+    welcomeMessage: "Welcome to my website"
+  });
+});
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}:${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile("server.log", log + "\n", err => {
+    if (err) {
+      console.log("Unable to append to server.log");
+    }
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render("maintenance.hbs");
+});
+
+// app.get("/", (req, res) => {
+//   res.render("home.hbs", {
+//     pageTitle: "Home Page",
+//     welcomeMessage: "Welcome to my website"
+//   });
+// });
+
+app.get("/about", (req, res) => {
+  res.render("about.hbs", {
+    pageTitle: "About Page"
+  });
+});
+app.get("/bad", (req, res) => {
+  res.send({
+    error: "bad request"
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is up on port 3000");
+});
+app.use(express.static(__dirname + "/public"));
+hbs.registerPartials(__dirname + "/views/partials"); 
